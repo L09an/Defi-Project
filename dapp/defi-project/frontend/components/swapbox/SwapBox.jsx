3d5cc299aab2cc7f22d199edd709f12817f9b55f
Jianyuan Zeng <loganzjy@gmail.com>
2023-04-08 10:48:42 +0000

    Add the swap box

--- dapp/defi-project/frontend/components/swapbox/SwapBox.jsx
+++ dapp/defi-project/frontend/components/swapbox/SwapBox.jsx
@@ -1,27 +1,26 @@
 // SwapBox.jsx
 import React from 'react';
-import styles from '../../styles/SwapBox.module.css'
+import styles from '../../styles/SwapBox.module.css';
 
-const SwapBox = () => {
+export default function SwapBox () {
   return (
-    <div className={styles.swapBox}>
+    <div className={styles.swapbox}>
       <h2>Swap</h2>
-      <div className={styles.swapInput}>
+      <div className={styles.swapinput}>
         <label htmlFor="fromToken">From</label>
         <input type="number" id="fromToken" placeholder="0.0" />
         {/* Replace with your token symbol */}
-        <span className={styles.tokenSymbol}>ETH</span>
+        <span className={styles.tokensymbol}>ETH</span>
       </div>
-      <div className={styles.swapArrow}>&#8595;</div>
-      <div className={styles.swapInput}>
+      <div className={styles.swaparrow}>&#8595;</div>
+      <div className={styles.swapinput}>
         <label htmlFor="toToken">To</label>
         <input type="number" id="toToken" placeholder="0.0" />
         {/* Replace with your token symbol */}
-        <span className={styles.tokenSymbol}>DAI</span>
+        <span className={styles.tokensymbol}>DAI</span>
       </div>
-      <button className={styles.swapButton}>Swap</button>
+      <button className={styles.swapbutton}>Swap</button>
     </div>
   );
 };
 
-export default SwapBox;
